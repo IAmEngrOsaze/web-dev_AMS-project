c1e07a61fed0df8be778194ffd08d090590e78f8
John Osaze <johnfavourosaze@gmail.com>
2022-11-11 09:17:56 +0100

    Completed all merging

---
+++ login-page/index.php
@@ -0,0 +1,64 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+ 
+<div>
+    <div class="fx" >HARRYSMART FX</div>
+</div>
+
+<div id='form'>
+<form class='form'>
+<div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Username</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" Placeholder='Enter username'>
+    
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Firstname</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" Placeholder='Enter firstnmae'>
+    
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Lastname</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" Placeholder='Enter lastname'>
+    
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email Address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" Placeholder='Enter email Address'>
+    
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" Placeholder='Enter password'>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Re-enter Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" Placeholder='Re-enter password'>
+  </div>
+
+  <button type="submit" class="btn btn-secondary">Sign Up</button>
+</form>
+</div>
+<hr>
+<footer class="section">
+    <div>&copy Copyright 2022 Edo Innovates</div>
+</footer>
+
+</body>
+</html>
+
